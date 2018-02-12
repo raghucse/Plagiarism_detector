86c54a1af3347403c6d3736b006ed8ae22e581ba
qujunhao1549 <qu.ju@husky.neu.edu>
2018-02-11 21:02:52 -0500

    update Faculty

--- phaseB/interfaces/Faculty.java
+++ phaseB/interfaces/Faculty.java
@@ -3,4 +3,14 @@ package plagiarismdetector;
 public interface Faculty extends User {
 	
 	public void runPlagiarismChecker(Assignment assignment);
+
+	@Override
+	default boolean login(String email, String password) {
+		return false;
+	}
+
+	@Override
+	default boolean logout() {
+		return false;
+	}
 }
