858bf20e010adf9a2a7491758cd508e0eabd290a
qujunhao1549 <qu.ju@husky.neu.edu>
2018-02-11 21:32:16 -0500

    update

--- phaseB/interfaces/Faculty.java
+++ phaseB/interfaces/Faculty.java
@@ -1,16 +1,10 @@
 package plagiarismdetector;
 
+import netscape.javascript.JSObject;
+
 public interface Faculty extends User {
 	
-	public void runPlagiarismChecker(Assignment assignment);
+	void runPlagiarismChecker(Assignment assignment);
 
-	@Override
-	default boolean login(String email, String password) {
-		return false;
-	}
-
-	@Override
-	default boolean logout() {
-		return false;
-	}
+	JSObject viewReport (Report report);
 }
