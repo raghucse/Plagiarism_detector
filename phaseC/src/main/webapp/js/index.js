c9cf3415063110b3b4c6517898fc2c5ed8f52838
qujunhao1549 <qu.ju@husky.neu.edu>
2018-03-01 14:47:00 -0500

    React can get the input

--- phaseC/src/main/webapp/js/index.js
+++ phaseC/src/main/webapp/js/index.js
@@ -1,83 +1,107 @@
-class Test extends React.Component {
+class Index extends React.Component {
+
+	/** 
+	 * Constructor
+	 * 
+	 * The state contains:
+	 *   page: to show the log_in page or the register page
+	 *   email: the user's email address
+	 *   password: the user's password
+	 *   cfrm_pwd: confirm the password for register
+	 *   role: which role is the user
+	*/
 	constructor() {
 		super();
-		this.state = { page: 0 }
+		this.state = {
+			page: 0,
+			email: '',
+			password: '',
+			cfrm_pwd: '',
+			role: ''
+		}
 	}
 
-    render() {
-    	if (this.state.page == 0) {
-    		return(
-    			<div >
-				<p>
-					<button className={ this.state.page == 1 ? "clickedButton" : "unclickedButton" }
-						onClick={ () => this.setState({ page: 1 })}>Register</button>
-					<button className={ this.state.page == 0 ? "clickedButton" : "unclickedButton" }
-						onClick={ () => this.setState({ page: 0 })}>Log In</button>
-					&nbsp;<br/>
-				</p>
-				<table>
-					<tr>
-						<td><span>Email</span></td>
-						<td><input type="text"/></td>
-					</tr>
-					<tr>
-						<td><span>Password</span></td>
-						<td><input type="password"/></td>
-					</tr>
-					<tr>
-						<td>
-						<input class="button" type="submit" value="Submit"/>
-						<input class="button" type="reset" value="Reset" />
-						</td>
-					</tr>
-					<tr>
-       				    <td colspan="2" align="center">
-              			<a href="#"><br />Forget password?</a>
-            			</td>
-          			</tr>
-				</table>
-				</div>
-			);
-		} else {	
+	// Submit the form to login
+	onLogInSubmit() {
+		alert(this.state.email);
+	}
+
+	// Submit the form to register
+	onRegisterSubmit() {
+		alert(this.state.password);
+	}
+	
+	// Render the UI
+	render() {
+		if (this.state.page == 0) {
 			return(
 				<div>
-				<p>
-					<button className={ this.state.page == 1 ? "clickedButton" : "unclickedButton" }
-						onClick={ () => this.setState({ page: 1 })}>Register</button>
-					<button className={ this.state.page == 0 ? "clickedButton" : "unclickedButton" }
-						onClick={ () => this.setState({ page: 0 })}>Log In</button>
-					&nbsp;<br/>
-				</p>
-				<table>
-          			<tr>
-            			<td><span>Email</span></td>
-            			<td><input type="email" /></td>
-          			</tr>
-          			<tr>
-            			<td><span>Password</span></td>
-            			<td><input type="password" /></td>
-          			</tr>
-          			<tr>
-            			<td><span>Confirm Password</span></td>
-            			<td><input type="password" /></td>
-          			</tr>
-          			<tr>
-            			<td><span>Role</span></td>
-            			<td>
-              				<select>
-                				<option value ="Professor">Professor</option>
-                				<option value ="Grader">Grader</option>
-                				<option value ="Admin">Administrator</option>
-              				</select>
-            			</td>
-          			</tr>
-          			<tr>
-            			<td colspan="2" align="center">
-              				<br /><input class="button" type="submit" value="Submit" />
-                  			<input class="button" type="reset" value="Reset" />
-            			</td>
-          			</tr>
-        			</table>
+					<p>
+						<button className={ this.state.page == 1 ? "clickedButton" : "unclickedButton" }
+							onClick={ () => this.setState({ page: 1 }) }>Register</button>
+						<button className={ this.state.page == 0 ? "clickedButton" : "unclickedButton" }
+							onClick={ () => this.setState({ page: 0 }) }>Log In</button>&nbsp;<br/>
+					</p>
+					<table>
+					<form onSubmit={ () => this.onLogInSubmit() }>
+						<tr>
+							<td><span>Email</span></td>
+							<td><input type="text" onChange={ ev => this.setState({ email: ev.target.value }) } /></td>
+						</tr>
+						<tr>
+							<td><span>Password</span></td>
+							<td><input type="password" onChange={ ev => this.setState({ password: ev.target.value }) } /></td>
+						</tr>
+						<tr>
+							<td><input class="button" type="submit" value="Submit"/></td>
+							<td><input class="button" type="reset" value="Reset" /></td>
+						</tr>
+						<tr>
+							<td colspan="2"><a href="#"><br />Forget password?</a></td>
+          	</tr>
+					</form>
+					</table>
+				</div>
+			);
+		} else {
+			return(
+				<div>
+					<p>
+						<button className={ this.state.page == 1 ? "clickedButton" : "unclickedButton" }
+							onClick={ () => this.setState({ page: 1 }) }>Register</button>
+						<button className={ this.state.page == 0 ? "clickedButton" : "unclickedButton" }
+							onClick={ () => this.setState({ page: 0 }) }>Log In</button>&nbsp;<br/>
+					</p>
+					<table>
+						<form onSubmit={ () => this.onRegisterSubmit() }>
+						<tr>
+							<td><span>Email</span></td>
+							<td><input type="email" onChange={ ev => this.setState({ email: e.target.value }) } /></td>
+          	</tr>
+						<tr>
+							<td><span>Password</span></td>
+            	<td><input type="password" onChange={ ev => this.setState({ password: ev.target.value }) } /></td>
+          	</tr>
+          	<tr>
+            	<td><span>Confirm Password</span></td>
+            	<td><input type="password" onChange={ ev => this.setState({ cfrm_pwd: ev.target.value }) } /></td>
+          	</tr>
+          	<tr>
+            	<td><span>Role</span></td>
+            	<td>
+              	<select>
+                	<option value ="Professor">Professor</option>
+                	<option value ="Grader">Grader</option>
+                	<option value ="Admin">Administrator</option>
+              	</select>
+            	</td>
+          	</tr>
+          	<tr>
+            	<td><input class="button" type="submit" value="Submit"/></td>
+              <td><input class="button" type="reset" value="Reset" /></td>
+						</tr>
+					</form>
+        	</table>
 				</div>
 			);
 		}
@@ -85,6 +109,6 @@ class Test extends React.Component {
 }
 
 ReactDOM.render(
-	<Test />,
+	<Index />,
 	document.getElementById('root')
 );
