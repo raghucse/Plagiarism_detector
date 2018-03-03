90bd2cbf19398ac1acd9a9252f65399df6fc9280
qujunhao1549 <qu.ju@husky.neu.edu>
2018-03-03 15:44:57 -0500

    Add test cases for two comparison strategies

---
+++ phaseC/src/test/java/edu/neu/ComparisonTest.java
@@ -0,0 +1,51 @@
+package edu.neu;
+
+import edu.neu.comparison.EditDistance;
+import edu.neu.comparison.LCS;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.logging.Logger;
+
+import static org.junit.Assert.assertEquals;
+
+public class ComparisonTest {
+
+    private String getSingleFile(String path) {
+        StringBuilder sb = new StringBuilder();
+        try (
+                FileInputStream fileInputStream = new FileInputStream(new File(path));
+                Reader reader = new InputStreamReader(fileInputStream)
+        ) {
+            int temp;
+            while ((temp = reader.read()) != -1) {
+                if (((char) temp) != '\r') {
+                    sb.append((char) temp);
+                }
+            }
+        } catch (Exception e) {
+            Logger logger = Logger.getLogger("Warning");
+            logger.warning(e.getMessage());
+        }
+        return sb.toString();
+    }
+
+    @Test
+    public void testLCS() {
+        String file1 = "src/main/resources/simplecodetree.txt";
+        String file2 = "src/main/resources/simplecode2tree.txt";
+        LCS lcs = new LCS(getSingleFile(file1), getSingleFile(file2));
+        assertEquals(679, lcs.getLength());
+    }
+
+    @Test
+    public void testEDist() {
+        String file1 = "src/main/resources/simplecodetree.txt";
+        String file2 = "src/main/resources/simplecode2tree.txt";
+        EditDistance distance = new EditDistance();
+        assertEquals(596, distance.editDist(getSingleFile(file1), getSingleFile(file2)));
+    }
+}
