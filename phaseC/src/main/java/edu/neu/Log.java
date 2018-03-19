851e834c5155eef90aafd4ac09101c9e7cffa525
Raghavendra Venkatesh <venkatesh.ra@husky.neu.edu>
2018-03-19 14:23:54 -0400

    CS106-37 #comment Initial commit

---
+++ phaseC/src/main/java/edu/neu/Log.java
@@ -0,0 +1,12 @@
+package edu.neu;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class Log {
+    private static final Logger logger = LogManager.getLogger(Log.class);
+
+    public  static void info(String message){
+        logger.info(message);
+    }
+}
