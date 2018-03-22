46cc55b4e03442cfeb0ddd5d8338fdb6fb56829a
qujunhao1549 <qu.ju@husky.neu.edu>
2018-03-22 01:08:15 -0400

    CS106-46 #comment Add run method

--- phaseC/src/main/webapp/js/professor.js
+++ phaseC/src/main/webapp/js/professor.js
@@ -5,7 +5,8 @@ class Check extends React.Component {
 	 * 
 	 * The state contains:
 	 *   runs: list of all runs' ID
-	 *   statistics: ID of the selected run to check its data 
+	 *   statistics: ID of the selected run to check its data
+	 *   stuNum: the number of students in the run, by default is 1 
 	*/
 	constructor() {
 		/**
@@ -15,7 +16,8 @@ class Check extends React.Component {
 		var loadRuns = [0, 1, 2];
 		this.state = {
 			runs: loadRuns,
-			statistics: 0
+			statistics: 0,
+			stuNum: 1
 		}
 	}
 
@@ -88,6 +90,34 @@ class Check extends React.Component {
 	}
 
 	/**
+	 * Add student in the modal
+	 */
+	addStudent() {
+		let newStuNum = this.state.stuNum + 1;
+		this.setState({
+			stuNum: newStuNum
+		});
+
+		var newStudent = '<div className="row"><div className="col-md-5">Student '
+			+ this.state.stuNum
+			+ '</div><div className="col-md-5"><input type="text" id="'
+			+ this.state.stuNum.toString()
+			+ '"/></div></div>';
+		document.getElementById('github_links').innerHTML += newStudent;
+	}
+
+	/**
+	 * Run the checks
+	 */
+	runCheck() {
+		var links = [];
+		for (var i = 1; i < this.state.stuNum; i++) {
+			links.push(document.getElementById(i.toString()).value);
+		}
+		console.log(links);
+	}
+
+	/**
 	 * Render the main UI
 	 */
 	render() {
@@ -107,8 +137,12 @@ class Check extends React.Component {
 								<div className="col-md-5">Run description</div>
 								<div className="col-md-5"><input type="text" id="run_description"/></div>
 							</div>
+
+							<div id="github_links"></div>
+
 							<div className="row">
-								<input type="submit" value="Run" id="run"/>
+								<div className="col-md-5"><button id="run" onClick={ this.runCheck.bind(this) }>Run</button></div>
+								<div className="col-md-5"><button id="run" onClick={ this.addStudent.bind(this) }>Add Student</button></div>
 							</div>
 						</div>
 					</div>
