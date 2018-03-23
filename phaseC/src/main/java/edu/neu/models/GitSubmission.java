44ed84cec4fa118e452351b234acb1f1c0669a46
Bharat V <vaidhyanathan.b@husky.neu.edu>
2018-03-23 16:10:35 -0400

    Implemented git clone

--- phaseC/src/main/java/edu/neu/models/GitSubmission.java
+++ phaseC/src/main/java/edu/neu/models/GitSubmission.java
@@ -1,12 +1,19 @@
 package edu.neu.models;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+
+import edu.neu.Log;
 
 public class GitSubmission implements Submission{
 	
 	private String gitURL;
+	private List<File> files;
 	
 	public GitSubmission(String gitURL) {
 		this.gitURL = gitURL;
@@ -14,7 +21,39 @@ public class GitSubmission implements Submission{
 
 	@Override
 	public List<File> getFiles() {
-		return new ArrayList<>();
+		// To implement git clone and return a list of files
+		
+		if(files==null) {
+			files = new ArrayList<>();
+			String remoteUrl = gitURL;
+			File localPath;
+			try {
+				localPath = File.createTempFile("jgitTemphelloworld", "");
+				
+				if(!localPath.delete()) {
+		            Log.error("Could not delete temporary file " + localPath);
+		        }
+				
+				// then clone
+				Log.info("Cloning from " + remoteUrl + " to " + localPath);
+		        try (Git result = Git.cloneRepository()
+		                .setURI(remoteUrl)
+		                .setDirectory(localPath)
+		                .call()) {
+		        		
+			        for(File f : localPath.listFiles())
+			        {
+			        		if(!f.getName().equals(".git"))
+			        			files.add(f);
+			        }
+		        }
+				
+			} catch (Exception e) {
+				Log.error("Error git cloning" + e.getStackTrace());
+			}
+		}
+
+		return files;
 	}
 
 }
---
+++ phaseC/src/test/java/edu/neu/GitSubmissionTest.java
@@ -0,0 +1,27 @@
+package edu.neu;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import edu.neu.models.GitSubmission;
+
+public class GitSubmissionTest {
+
+	@Test
+	public void test01() {
+		GitSubmission submission = new GitSubmission("https://github.com/bharat94/hello_world.git");
+		List<String> fileNames = new ArrayList<>();
+		for (File f : submission.getFiles()) {
+			fileNames.add(f.getName());
+		}
+		
+		assertEquals(1, fileNames.size());
+		assertEquals("README.md", fileNames.get(0));
+	}
+	
+}
