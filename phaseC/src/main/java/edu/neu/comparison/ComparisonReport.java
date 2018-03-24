23c6b500e5a80cb87dc5112f7a594171c735fc11
Bharat V <vaidhyanathan.b@husky.neu.edu>
2018-03-24 07:06:20 -0400

    CS106-64 #comment updated sonarlint warnings

--- phaseC/src/main/java/edu/neu/comparison/ComparisonReport.java
+++ phaseC/src/main/java/edu/neu/comparison/ComparisonReport.java
@@ -30,24 +30,15 @@ public class ComparisonReport implements Serializable{
 	}
 
 	double score;
-	//EnumMap<STRATEGIES, Double> scores;
 	
 	public ComparisonReport() {
-		//scores = new EnumMap<>(STRATEGIES.class);
 	}
 	
 	public ComparisonReport(String filename1, String filename2, double score) {
 		this.filename1 = filename1;
 		this.filename2 = filename2;
 		this.score = score;
-		//scores = new EnumMap<>(STRATEGIES.class);
 	}
-
-	/*
-	public void putScore(STRATEGIES strategy, double score) {
-		scores.put(strategy, score);
-	}
-	*/
 	
 	@Override
 	public String toString() {
@@ -58,14 +49,12 @@ public class ComparisonReport implements Serializable{
 		out.writeUTF(filename1);
 		out.writeUTF(filename2);
 		out.writeDouble(score);
-		//out.writeObject(scores);
 	}
 	
 	private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
 		filename1 = in.readUTF();
 		filename2 = in.readUTF();
 		score = in.readDouble();
-		//scores = (EnumMap<STRATEGIES, Double>) in.readObject();
 	}
 	
 }
--- phaseC/src/main/java/edu/neu/comparison/LCS.java
+++ phaseC/src/main/java/edu/neu/comparison/LCS.java
@@ -60,22 +60,23 @@ public class LCS implements ASTBasedStrategy{
         int tree1Size = tree1.size();
         int tree2Size = tree2.size();
         int cost;
-        double score, maxNodeCount;
-        int LCSScore[][] = new int[tree1Size+1][tree2Size+1];
+        double score;
+        double maxNodeCount;
+        int[][] lCSScore = new int[tree1Size+1][tree2Size+1];
 
         for(int i=0; i<=tree1Size; i++)
         {
             for(int j = 0; j<=tree2Size; j++)
             {
                 if(i == 0 || j == 0)
-                    LCSScore[i][j] = 0;
+                	lCSScore[i][j] = 0;
                 else if(tree1.get(i-1).equals(tree2.get(j-1)))
-                    LCSScore[i][j] = LCSScore[i-1][j-1]+1;
+                	lCSScore[i][j] = lCSScore[i-1][j-1]+1;
                 else
-                    LCSScore[i][j] = Math.max(LCSScore[i-1][j], LCSScore[i][j-1]);
+                	lCSScore[i][j] = Math.max(lCSScore[i-1][j], lCSScore[i][j-1]);
             }
         }
-        cost =  LCSScore[tree1Size][tree2Size];
+        cost =  lCSScore[tree1Size][tree2Size];
         maxNodeCount =Math.max(tree1Size,tree2Size);
         score = cost/maxNodeCount;
         return score;
