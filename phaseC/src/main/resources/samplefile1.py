c2328176e95b30b7258835db12718d5fa5801456
anantaarora12 <arora.ana@husky.neu.edu>
2018-03-24 02:00:56 -0400

    CS106-69 #adding test cases for the comparison package

---
+++ phaseC/src/main/resources/samplefile1.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+def createDict():
+    dict1 = {1:'ananta', 2:'raghu', 3:'junhao', 4:'bharat'}
+
+    for key in dict1.keys():
+        print(dict1[key])
+
+
+    for x in range(10):
+        print(x)
+
+
+    for y in range(20):
+        print(y)
+
+    while(true):
+        a = 8 + 12
+
+    if(a == 20):
+        x = a
+    else:
+        x = 100
+
+
+createDict()
