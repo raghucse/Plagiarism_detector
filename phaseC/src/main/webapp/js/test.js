b8dc3093704ff7d1a3f9653be7304235fdd9ac39
qujunhao1549 <qu.ju@husky.neu.edu>
2018-03-27 15:03:38 -0400

    CS106-72 #comment Initialize the single page application.

---
+++ phaseC/src/main/webapp/js/test.js
@@ -0,0 +1,16 @@
+class Application extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return(
+      <div>sa</div>
+    );
+  }
+}
+
+ReactDOM.render(
+	<Application />,
+	document.getElementById('root')
+);
