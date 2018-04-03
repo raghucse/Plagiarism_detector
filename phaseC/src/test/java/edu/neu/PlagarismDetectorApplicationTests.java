7ea8fa69a9b1adb8552931d6eb64262b12d45a07
Raghavendra Venkatesh <venkatesh.ra@husky.neu.edu>
2018-04-02 20:25:36 -0400

    CS106-74 #comment login test case

--- phaseC/src/test/java/edu/neu/PlagarismDetectorApplicationTests.java
+++ phaseC/src/test/java/edu/neu/PlagarismDetectorApplicationTests.java
@@ -7,13 +7,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import edu.neu.reports.PlagiarismRunRequest;
 import org.junit.Test;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class PlagarismDetectorApplicationTests extends AbstractMvc{
@@ -46,17 +41,7 @@ public class PlagarismDetectorApplicationTests extends AbstractMvc{
 		assertEquals("Plagiarism run started",result.andReturn().getResponse().getContentAsString());
 
 		//get reports by id
-		String res = "[{\"id\":1,\"name\":\"Sample description\",\"owner\":1,\"reportScore\":0.1612665072227597," +
-				"\"reportFile\":{\"reportMesTesting the loginsage\":\"Report Content\",\"comparisonList\":[{\"filename1\"" +
-				":\"student1-file1.py\",\"filename2\":\"student2-file2.py\",\"scores\":{\"totalScore\"" +
-				":0.1612665072227597,\"subScores\":\"LCS:0.2087912087912088; LVDistance:0.20879120879120883; " +
-				"CosineSimilarity:0.06621710408586144; \"}},{\"filename1\":\"student1-file1.py\",\"filename2\":" +
-				"\"student2-file1.py\",\"scores\":{\"totalScore\":0.1695617381920277,\"subScores\"" +
-				":\"LCS:0.2087912087912088; LVDistance:0.20879120879120883; CosineSimilarity:0.09110279699366539; \"}}" +
-				",{\"filename1\":\"student1-file2.py\",\"filename2\":\"student2-file2.py\",\"scores\":{\"totalScore\":1.0," +
-				"\"subScores\":\"LCS:1.0; LVDistance:1.0; CosineSimilarity:1.0; \"}},{\"filename1\":\"student1-file2.py\"" +
-				",\"filename2\":\"student2-file1.py\",\"scores\":{\"totalScore\":0.8168489401184441,\"subScores\"" +
-				":\"LCS:0.9615384615384616; LVDistance:0.9615384615384616; CosineSimilarity:0.5274698972784089; \"}}]}}]";
+		String res = "[{\"id\":1,\"name\":\"Sample description\",\"owner\":1,\"reportScore\":0.1612665072227597,\"reportFile\":{\"reportMessage\":\"Report Content\",\"comparisonList\":[{\"filename1\":\"student1-file1.py\",\"filename2\":\"student2-file2.py\",\"scores\":{\"totalScore\":0.1612665072227597,\"subScores\":\"LCS:0.2087912087912088; LVDistance:0.20879120879120883; CosineSimilarity:0.06621710408586144; \"}},{\"filename1\":\"student1-file1.py\",\"filename2\":\"student2-file1.py\",\"scores\":{\"totalScore\":0.1695617381920277,\"subScores\":\"LCS:0.2087912087912088; LVDistance:0.20879120879120883; CosineSimilarity:0.09110279699366539; \"}},{\"filename1\":\"student1-file2.py\",\"filename2\":\"student2-file2.py\",\"scores\":{\"totalScore\":1.0,\"subScores\":\"LCS:1.0; LVDistance:1.0; CosineSimilarity:1.0; \"}},{\"filename1\":\"student1-file2.py\",\"filename2\":\"student2-file1.py\",\"scores\":{\"totalScore\":0.8168489401184441,\"subScores\":\"LCS:0.9615384615384616; LVDistance:0.9615384615384616; CosineSimilarity:0.5274698972784089; \"}}]}}]";
 
 		ResultActions resultReport = mockMvc.perform(get("/report/user/reportIds/{userId}", "1")
 						.header("Authorization", getToken()));
