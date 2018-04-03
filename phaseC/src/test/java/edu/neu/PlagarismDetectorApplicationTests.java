5751436e60f36be1dc66b587a6d456e13555ef74
Raghavendra Venkatesh <venkatesh.ra@husky.neu.edu>
2018-04-02 20:16:41 -0400

    CS106-74 #comment Adding more test cases.

--- phaseC/src/test/java/edu/neu/PlagarismDetectorApplicationTests.java
+++ phaseC/src/test/java/edu/neu/PlagarismDetectorApplicationTests.java
@@ -4,6 +4,7 @@ package edu.neu;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import edu.neu.reports.PlagiarismRunRequest;
@@ -33,25 +34,34 @@ public class PlagarismDetectorApplicationTests extends AbstractMvc{
 
 	@Test
 	public void testRunPlagiarism() throws Exception {
-		List<String> gitUrls = new ArrayList<>();
 
-		gitUrls.add("https://github.com/bharat94/testRepo1.git");
-		gitUrls.add("https://github.com/bharat94/testRepo2.git");
 
-		PlagiarismRunRequest plagRun = new PlagiarismRunRequest();
-		plagRun.setDescription("Sample run");
-		plagRun.setGitUrls(gitUrls);
-        plagRun.setSharedUsers(new ArrayList<>());
-		System.out.println("Git utrl:: "+json(gitUrls));
 		ResultActions result = mockMvc.perform(
 				post("/plagiarism/run")
 						.header("Authorization", getToken())
-						.param("description",plagRun.getDescription())
+						.param("description","Sample description")
 						.param("gitUrls", "https://github.com/bharat94/testRepo1.git, https://github.com/bharat94/testRepo2.git")
-						.param("sharedUsers", json(new ArrayList<>()))	);
+						.param("sharedUsers", ""));
 		Thread.sleep(5000);
 		assertEquals("Plagiarism run started",result.andReturn().getResponse().getContentAsString());
 
+		//get reports by id
+		String res = "[{\"id\":1,\"name\":\"Sample description\",\"owner\":1,\"reportScore\":0.1612665072227597," +
+				"\"reportFile\":{\"reportMesTesting the loginsage\":\"Report Content\",\"comparisonList\":[{\"filename1\"" +
+				":\"student1-file1.py\",\"filename2\":\"student2-file2.py\",\"scores\":{\"totalScore\"" +
+				":0.1612665072227597,\"subScores\":\"LCS:0.2087912087912088; LVDistance:0.20879120879120883; " +
+				"CosineSimilarity:0.06621710408586144; \"}},{\"filename1\":\"student1-file1.py\",\"filename2\":" +
+				"\"student2-file1.py\",\"scores\":{\"totalScore\":0.1695617381920277,\"subScores\"" +
+				":\"LCS:0.2087912087912088; LVDistance:0.20879120879120883; CosineSimilarity:0.09110279699366539; \"}}" +
+				",{\"filename1\":\"student1-file2.py\",\"filename2\":\"student2-file2.py\",\"scores\":{\"totalScore\":1.0," +
+				"\"subScores\":\"LCS:1.0; LVDistance:1.0; CosineSimilarity:1.0; \"}},{\"filename1\":\"student1-file2.py\"" +
+				",\"filename2\":\"student2-file1.py\",\"scores\":{\"totalScore\":0.8168489401184441,\"subScores\"" +
+				":\"LCS:0.9615384615384616; LVDistance:0.9615384615384616; CosineSimilarity:0.5274698972784089; \"}}]}}]";
+
+		ResultActions resultReport = mockMvc.perform(get("/report/user/reportIds/{userId}", "1")
+						.header("Authorization", getToken()));
+		resultReport.andExpect(status().isOk());
+		assertEquals(res,resultReport.andReturn().getResponse().getContentAsString());
 
 	}
 
