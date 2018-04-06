f84010794d94acc3143af628e1fe5c777af58929
Bharat V <vaidhyanathan.b@husky.neu.edu>
2018-04-06 16:48:45 -0400

    Added comments for PlagiarismChecker

--- phaseC/src/main/java/plagiarismdetection/PlagiarismChecker.java
+++ phaseC/src/main/java/plagiarismdetection/PlagiarismChecker.java
@@ -18,21 +18,52 @@ import edu.neu.reports.Report;
 import edu.neu.reports.ReportService;
 import edu.neu.utils.Constants;
 
+/**
+ * This is a runnable class which is run by the detection executor
+ * Whenever a plagiarism check is initiated, it is constructed as this runnable and then
+ * executed in the thread pool executor service
+ * 
+ * The runnable takes in a plagiarism run obect and a comparison strategy and then executes
+ * as required.
+ * 
+ * The runnable includes a logger for logging.
+ * 
+ * @author bharat vaidhyanathan (team 106)
+ */
 public class PlagiarismChecker implements Runnable{
 	
+	// The logger for this class 
 	private static final Logger log = LogManager.getLogger(PlagiarismChecker.class);
 
 	// A single plagiarism check is run when a plagiarism check is initiated
 	private PlagiarismRun plagiarismRun;
+	
+	// The report that is generated
 	private Report report;
+	
+	// The content of the report
 	private ReportContent reportContent;
+	
+	// The comparison strategy to be used
 	private Strategy comparisonStrategy;
+	
+	// booleans signifying the process completion
 	private boolean runStarted = false;
 	private boolean runCompleted = false;
+	
+	// The check Id that is randomly generated
 	private String checkID;
 	
+	// The report service that is used to interact with the report
 	private ReportService reportService;
 	
+	/**
+	 * The constructor for the plagiarism checker
+	 * @param plagiarismRun : the plagiarism run object to be used to run the check
+	 * @param comparisonStrategy : the comparison strategy that would be used
+	 * @param reportService : the report service used to generat and dump reports
+	 * @param checkID : the check ID unique to this run
+	 */
 	public PlagiarismChecker(PlagiarismRun plagiarismRun, Strategy comparisonStrategy, ReportService reportService, String checkID) {
 		this.plagiarismRun = plagiarismRun;
 		this.comparisonStrategy = comparisonStrategy;
@@ -41,18 +72,28 @@ public class PlagiarismChecker implements Runnable{
 		this.checkID = checkID;
 	}
 	
+	/**
+	 * The run method
+	 * This is called by the executor service whenever it schedules this to a thread
+	 */
 	@Override
 	public void run() {
 		check();
 	}
 	
 	
+	/**
+	 * This method initiates the check and returns if the check was done or not
+	 * The method returns false if for some reason it is not able to do the check
+	 * @return : whether the check was successfully completed or not
+	 */
 	// returns the plagiarism check id assosciated with the run
 	public boolean check() {
 		if(this.plagiarismRun!=null) {
 			log.info("Initiating Plagiarism Check with id : "+checkID);
 			this.report = reportService.createNewEmptyReportWithNameAndOwner(plagiarismRun.getDescription(), plagiarismRun.getUserId());
 			runStarted = true;
+			// Starting to compare all submissions
 			this.compareAllSubmissions(this.plagiarismRun);
 			report.setReportFile(reportContent);
 			Log.info("Report generated with "+reportContent.getComparisonList().size()+" comparisons");
@@ -71,19 +112,32 @@ public class PlagiarismChecker implements Runnable{
 		return runCompleted;
 	}
 	
+	/**
+	 * This method compares all the submission for a plagiarism run
+	 * @param plagiarismRun : The plagiarism run object to check for
+	 */
 	private void compareAllSubmissions(PlagiarismRun plagiarismRun) {
 		Log.info("Start Comparing All Submissions pairs");
+		// Looping over all the submission pairs in the run
 		for(Submission[] submissionPair : getSubmissionPairs(plagiarismRun)) {
 			compareSubmissions(submissionPair[0], submissionPair[1]);
 		}
 		Log.info("Done Comparing All Submissions pairs");
 	}
 	
+	/**
+	 * A method to return unique submission pairs from the submission list in the 
+	 * plagiarism run object
+	 * @param plagiarismRun : the plagiarism run objec inside which the submissions are
+	 * @return : Unique pairs from the list of submissions in the run object
+	 */
 	public List<Submission[]> getSubmissionPairs(PlagiarismRun plagiarismRun) {
 		List<Submission[]> result = new ArrayList<>();
+		// convert the submissions list to array
 		Submission[] submissions= plagiarismRun.getStudentSubmissions()
 				.toArray(new Submission[plagiarismRun.getStudentSubmissions().size()]);
 		
+		// Iterate over the array and return pairs  (unique)
 		for(int i=0; i<submissions.length-1; i++) {
 			for(int j=i+1; j<submissions.length; j++) {
 				result.add(new Submission[] {submissions[i], submissions[j]});
@@ -92,6 +146,11 @@ public class PlagiarismChecker implements Runnable{
 		return result;
 	}
 	
+	/**
+	 * 
+	 * @param submission1
+	 * @param submission2
+	 */
 	public void compareSubmissions(Submission submission1, Submission submission2) {
 		Log.info("Getting submission Files and comparing them");
 		for(File f1 : submission1.getFiles()) {
